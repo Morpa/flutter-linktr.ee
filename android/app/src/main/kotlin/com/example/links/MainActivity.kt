4f0bc8de2bf8f0dde493eeba16ebb31c63d0d782
morpa <andremorpa@gmail.com>
2023-02-18 07:58:41 +0000

    initial config

---
+++ android/app/src/main/kotlin/com/example/links/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.links
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/animation.dart';
+
+Color mainColor = const Color(0xffEFF0F3);
+Color primaryColor = const Color(0xffE4E7F1);
+Color secondaryColor = const Color(0xffFCFBFC);
+Color tertiaryColor = const Color(0xffFCFBFC);
+Color buttonColor = const Color(0xff09ADF3);
